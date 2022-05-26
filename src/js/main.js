450996b723daabe3a362ebaa49a1cd0405707fff
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2022-05-26 19:29:39 +0600

    js toggle

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
-$(function () {
-    
-});
+function toggle() {
+  if (
+    window.innerWidth <= 1000 &&
+    document.querySelectorAll('.toggle').length < 1
+  ) {
+    let div = document.createElement('div')
+    div.className = 'toggle'
+
+    const promoContainer = document.querySelector('.promo__container')
+    promoContainer.append(div)
+
+    div.innerHTML =
+      '<div class="promo__langs"><button type="button" class="promo__langs-item active">RU</button><button type="button" class="promo__langs-item">KZ</button></div>'
+  }
+  if (window.innerWidth > 1000) {
+    document.querySelector('.toggle').remove()
+  }
+}
+window.addEventListener('resize', toggle)
+
+document.addEventListener('DOMContentLoaded', toggle)
